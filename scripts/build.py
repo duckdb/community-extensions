32ab121bbf2146ff4863af948534d24e1514667b
Hannes Mühleisen <hannes@duckdblabs.com>
2024-06-03 13:48:36 +0200

    checkou

--- scripts/build.py
+++ scripts/build.py
@@ -1,2 +1,21 @@
 import os
-print(os.environ['ALL_CHANGED_FILES'])
+import yaml
+
+# TODO: check prefix, needs to be in installation dir
+
+desc_files = os.environ['ALL_CHANGED_FILES'].split(' ')
+if len(desc_files) != 1:
+	raise ValueError('cannot have multiple descriptors changed or packages with spaces in their names')
+desc_file = desc_files[0]
+
+with open(desc_file, 'r') as stream:
+    desc = yaml.safe_load(stream)
+
+print(desc)
+
+# todo check other stuff like build system etc.
+
+
+os.environ['COMMUNITY_EXTENSION_URL'] = desc['repo']['url']
+os.environ['COMMUNITY_EXTENSION_REVISION'] = desc['repo']['revision']
+os.environ['COMMUNITY_EXTENSION_NAME'] = desc['extension']['name']
