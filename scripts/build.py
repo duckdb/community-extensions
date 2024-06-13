ef8e66beda5d04a870e90adc4e453bd92fab87c5
Sam Ansmink <samansmink@hotmail.com>
2024-06-13 14:20:11 +0200

    its yml not yaml, clean script

--- scripts/build.py
+++ scripts/build.py
@@ -1,17 +1,33 @@
 import os
+import sys
 import yaml
 
 # TODO: check prefix, needs to be in installation dir
 
-desc_files = os.environ['ALL_CHANGED_FILES'].split(' ')
-if len(desc_files) != 1:
+if 'ALL_CHANGED_FILES' in os.environ:
+	desc_files = os.environ['ALL_CHANGED_FILES'].split(' ')
+else:
+	desc_files = []
+
+print(f"Files changed: {desc_files}")
+
+if len(desc_files) > 1:
 	raise ValueError('cannot have multiple descriptors changed or packages with spaces in their names')
+
+if len(desc_files) == 0 or len(desc_files[0]) == 0:
+	print("No changed files, nothing will be built")
+	with open('env.sh', 'w+') as hdl:
+		hdl.write(f"COMMUNITY_EXTENSION_GITHUB=\n")
+		hdl.write(f"COMMUNITY_EXTENSION_REF=\n")
+		hdl.write(f"COMMUNITY_EXTENSION_NAME=\n")
+		sys.exit(os.EX_OK)
+
 desc_file = desc_files[0]
 if len(desc_file) == 0:
 	raise ValueError('description file not found (ALL_CHANGED_FILES was set but empty)')
 
 with open(desc_file, 'r') as stream:
-    desc = yaml.safe_load(stream)
+	desc = yaml.safe_load(stream)
 
 print(desc)
 
