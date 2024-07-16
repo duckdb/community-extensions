69f478e7dc359afc0f5c4953958166b7c188a353
Sam Ansmink <samansmink@hotmail.com>
2024-07-16 15:43:39 +0200

    pass through vcpkg commit

--- scripts/build.py
+++ scripts/build.py
@@ -36,9 +36,12 @@ with open('env.sh', 'w+') as hdl:
 	hdl.write(f"COMMUNITY_EXTENSION_NAME={desc['extension']['name']}\n")
 	excluded_platforms = desc['extension'].get('excluded_platforms')
 	requires_toolchains = desc['extension'].get('requires_toolchains')
+	vcpkg_commit = desc['extension'].get('vcpkg_commit')
 	if excluded_platforms:
 		hdl.write(f"COMMUNITY_EXTENSION_EXCLUDE_PLATFORMS={excluded_platforms}\n")
 	if requires_toolchains:
 		hdl.write(f"COMMUNITY_EXTENSION_REQUIRES_TOOLCHAINS={requires_toolchains}\n")
+	if vcpkg_commit:
+		hdl.write(f"COMMUNITY_EXTENSION_VCPKG_COMMIT={vcpkg_commit}\n")
 	if deploy:
 		hdl.write(f"COMMUNITY_EXTENSION_DEPLOY=1\n")
